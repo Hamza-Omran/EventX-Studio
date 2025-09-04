edcb6b7948d0efc129b7e74a48724674f2aad25b
Hamza_Omran <hamza127444@gmail.com>
2025-09-04 19:45:19 +0300

    upload

--- frontend/src/components/DashboardRedirect/DashboardRedirect.jsx
+++ frontend/src/components/DashboardRedirect/DashboardRedirect.jsx
@@ -14,10 +14,10 @@ const DashboardRedirect = () => {
     }, [userInfo, navigate]);
 
     return (
-        <div style={{ 
-            display: "flex", 
-            justifyContent: "center", 
-            alignItems: "center", 
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
             height: "100vh",
             fontSize: "1.2rem",
             color: "#666"
